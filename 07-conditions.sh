e46d4d13ddedad55f987d661bd93d3c6ca3b1a11
DATTA BAVISETTI <dattabavisetti6304@gmail.com>
2026-02-06 12:00:46 +0530

    New Changes

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -3,9 +3,9 @@
 echo "Please enter number: "
 read NUM
 
-if [ $NUM -gt 20]; then
+if [ $NUM -gt 20 ];then
     echo "Given number $NUM is greater than 20"
-elif [ $NUM -eq 20]; then
+elif [ $NUM -eq 20 ];then
     echo "Given number $NUM is equal to 20"
 else
     echo "Given number $NUM is not greater than 20"
