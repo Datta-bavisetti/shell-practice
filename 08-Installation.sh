dbf945d7578a0bedbf7e9e124d5530d471f4f810
DATTA BAVISETTI <dattabavisetti6304@gmail.com>
2026-02-08 11:38:48 +0530

    New Changes

---
+++ 08-Installation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#To install any package, we have to run with sudo access. But not all the users use the same. So, let's validate
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run the script with root user"
+fi
+
+echo "Installing Nginx"
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx...FAILURE"
+else
+    echo "Installing Nginx...SUCCESS"
+fi
