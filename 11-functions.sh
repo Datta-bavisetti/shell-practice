f716aebaf7c6904940e414ce747340ab1b0402ff
DATTA BAVISETTI <dattabavisetti6304@gmail.com>
2026-02-09 10:15:55 +0530

    New Changes

---
+++ 11-functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#To install any package, we have to run with sudo access. But not all the users use the same. So, let's validate
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run the script with root user"
+    exit 1
+fi
+
+UNINSTALL(){
+if [ $1 -ne 0 ]; then
+    echo "$2...FAILURE"
+    exit 1
+else
+    echo "$2...SUCCESS"
+fi
+}
+
+dnf remove nginx -y
+INSTALL $? "Removing Nginx" #Here $? is 1st parameter and "---" is second parameter
+
+dnf remove mysql -y
+INSTALL $? "Removing MySQL" #Here $? is 1st parameter and "---" is second parameter
+
+dnf remove python -y
+INSTALL $? "Removing python" #Here $? is 1st parameter and "---" is second parameter
