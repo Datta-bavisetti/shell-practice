8a1269f1c486cbe489dbbd90e6e486601b385c10
DATTA BAVISETTI <dattabavisetti6304@gmail.com>
2026-02-09 11:52:19 +0530

    New Changes

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+
 #Print the numbers in given range
-echo "Please enter the number range to print up to: "
-read NUM
-for i in $(seq 1 "$NUM")
+#Method-1
+for i in {1..50}
 do
-    echo $i
+    echo i
 done
+
+
+#Print the numbers in given range
+#Method-2
+# echo "Please enter the number range to print up to: "
+# read NUM
+# for i in $(seq 1 "$NUM")
+# do
+#     echo $i
+# done
